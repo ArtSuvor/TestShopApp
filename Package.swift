6fdf0f4e4d5fdbd2c1dcf7adc54bbbe4867ab0ae
ArtSuvor <85563543+ArtSuvor@users.noreply.github.com>
2022-08-18 18:42:43 +0300

    fix

--- Package.swift
+++ Package.swift
@@ -5,24 +5,36 @@ import PackageDescription
 
 let package = Package(
     name: "TestShopApp",
+    defaultLocalization: "ru",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "TestShopApp",
             targets: ["TestShopApp"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git",
+            .upToNextMajor(from: "5.4.3")),
+        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk", from: "9.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TestShopApp",
-            dependencies: []),
+            dependencies: [
+                "Alamofire",
+                .product(name: "FirebaseAnalytics", package: "Firebase"),
+                .product(name: "FirebaseCrashlytics", package: "Firebase")
+            ],
+            path: "TestShopApp",
+            resources: [
+                .copy("Resources")
+            ]),
         .testTarget(
             name: "TestShopAppTests",
-            dependencies: ["TestShopApp"]),
+            dependencies: ["TestShopApp"],
+            path: "TestShopAppTests"),
     ]
 )
--- Sources/TestShopApp/TestShopApp.swift
+++
@@ -1,6 +0,0 @@
-public struct TestShopApp {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
